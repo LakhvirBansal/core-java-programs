06b56238d621d9ca2fece6d13616bda953255afd
Lakhvir Bansal <bansal.lucky007@gmail.com>
2018-07-06 11:12:36 +0530

    add new programs related to stack

---
+++ src/Stack/StackOperation.java
@@ -0,0 +1,12 @@
+package Stack;
+
+public class StackOperation {
+
+	public static void main(String[] args) {
+		Stack s = new Stack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.pop() + " Popped from stack");
+	}
+}
