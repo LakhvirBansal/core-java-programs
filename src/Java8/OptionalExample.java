8dec6d2f0d53b058d93ee73b5850048e7a2b2edd
Lakhvir Bansal <bansal.lucky007@gmail.com>
2018-08-24 14:25:35 +0530

    java8 optional class Example

---
+++ src/Java8/OptionalExample.java
@@ -0,0 +1,37 @@
+package Java8;
+
+import java.util.Optional;
+
+public class OptionalExample {
+
+	public static void main(String[] args) {
+		OptionalExample optionalExample = new OptionalExample();
+		Integer value1 = null;
+		Integer value2 = new Integer(10);
+
+		// Optional.ofNullable - allows passed parameter to be null.
+		Optional<Integer> a = Optional.ofNullable(value1);
+
+		// Optional.of - throws NullPointerException if passed parameter is null
+		Optional<Integer> b = Optional.of(value2);
+		System.out.println(optionalExample.sum(a, b));
+	}
+
+	private Integer sum(Optional<Integer> a, Optional<Integer> b) {
+		// Optional.isPresent - checks the value is present or not
+
+		System.out.println("First parameter is present: " + a.isPresent());
+		a.ifPresent(System.out::println);
+		System.out.println(a.orElse(50));
+		System.out.println("Second parameter is present: " + b.isPresent());
+		b.ifPresent(System.out::println);
+
+		// Optional.orElse - returns the value if present otherwise returns
+		// the default value passed.
+		Integer value1 = a.orElse(new Integer(0));
+
+		// Optional.get - gets the value, value should be present
+		Integer value2 = b.get();
+		return value1 + value2;
+	}
+}
